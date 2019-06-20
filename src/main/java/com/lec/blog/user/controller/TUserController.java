2c635a5f5f30771f0c47e1eb6666a755d337b1d2
lec <lecason.g@icloud.com>
2019-06-20 17:19:35 +0800

    mybatis-plus 代码生成器

---
+++ src/main/java/com/lec/blog/user/controller/TUserController.java
@@ -0,0 +1,20 @@
+package com.lec.blog.user.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author lec
+ * @since 2019-06-20
+ */
+@RestController
+@RequestMapping("/user/t-user")
+public class TUserController {
+
+}
---
+++ src/main/java/com/lec/blog/user/service/ITUserService.java
@@ -0,0 +1,16 @@
+package com.lec.blog.user.service;
+
+import com.lec.blog.user.entity.TUser;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author lec
+ * @since 2019-06-20
+ */
+public interface ITUserService extends IService<TUser> {
+
+}
